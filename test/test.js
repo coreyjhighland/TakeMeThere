51d98a23d4fcab1a154e4f0c1441ef48b05b2318
Corey Highland <coreyjhighland@gmail.com>
2018-09-26 12:16:48 -0400

    installed devDependencies

--- test/test.js
+++ test/test.js
@@ -1,10 +1,8 @@
-'use strict'
+'use strict';
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
 chai.use(chaiHttp);
 
-function hitRootUrl() {
-    
-}
+function hitRootUrl() {}
